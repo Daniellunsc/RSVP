a16cdc007d26a7296aa0a8d22359a12e49a05061
Daniel Luna <daniellunsc@gmail.com>
2017-09-28 14:55:55 -0300

    Criacao do componente de convidados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import Guestlist from './Components/GuestList.js';
 
 class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: 'Treasure',
+        isConfirmed: false
+      },
+      {
+        name: 'Daniel',
+        isConfirmed: true
+      },
+      {
+        name: 'Lorrane',
+        isConfirmed: true
+      },
+    ]
+  }
+
+  getTotalInvite = () => this.state.guests.length;
+  // getConfirmedGuests = () =>
+  // getUnconfirmedGuests = () =>
+
   render() {
     return (
       <div className="App">
@@ -36,32 +59,7 @@ class App extends Component {
             </tr>
           </tbody>
         </table>
-        <ul>
-          <li className="pending"><span>Safia</span></li>
-          <li className="responded"><span>Iver</span>
-            <label>
-              <input type="checkbox" checked /> Confirmed
-            </label>
-            <button>edit</button>
-            <button>remove</button>
-          </li>
-          <li className="responded">
-            <span>Corrina</span>
-            <label>
-              <input type="checkbox" checked /> Confirmed
-            </label>
-            <button>edit</button>
-            <button>remove</button>
-          </li>
-          <li>
-            <span>Joel</span>
-            <label>
-              <input type="checkbox" /> Confirmed
-            </label>
-            <button>edit</button>
-            <button>remove</button>
-          </li>
-        </ul>
+        <Guestlist guests= {this.state.guests}/>
       </div>
     </div>
     );
